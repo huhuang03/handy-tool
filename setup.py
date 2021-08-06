9bb9d0650110e934bc0f168bab631a0cc8a9a924
huhuang03 <huhuang03@gmail.com>
2021-08-06 15:17:30 +0800

    add done

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def _create_by_cli():
             print(f)
 
 
-COMMANDS = ["gitup", 'add_to_path', "gettopactivity", "ip", "cmake_ex", "merged_rm",
+COMMANDS = ["gitup", 'done', 'add_to_path', "gettopactivity", "ip", "cmake_ex", "merged_rm",
             "lg", 'utf8_2_utf8bom', "save_space", 'find_program', 'rn_ex', 'json2bean', 'jt_code', 'remote']
 
 
