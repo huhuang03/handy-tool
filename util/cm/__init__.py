a5946de54f2f8a49819e3d81bde0073b930f07fa
york <york@duomai.com>
2021-08-31 22:38:13 +0800

    so what's the action='store_true'

--- util/cm/__init__.py
+++ util/cm/__init__.py
@@ -1,19 +1,6 @@
-import os
-import pathlib
 import argparse
 from .clean import clean
-from ..util.util_git import add_git_ignore
-
-
-def _install():
-    # how can we do the install?
-    add_git_ignore('*build*/')
-    pathlib.Path('build32').mkdir(exist_ok=True)
-    os.system('cd build32')
-    os.system('cmake . -A win32')
-    os.system('cmake --build . --config Release --target install')
-    os.system('cmake --build . --config Debug --target install')
-    os.system('cd ..')
+from .init import install
 
 
 def main():
@@ -23,11 +10,12 @@ def main():
     subparsers.add_parser('clean', help='clean the cmake cache files.')
     subparsers.add_parser('c', help='clean the cmake cache files.')
 
-    subparsers.add_parser('i', help='install 32 bit')
+    p_i = subparsers.add_parser('i', help='install 32 bit')
+    p_i.add_argument('-d', dest='build_debug', action='store_true')
 
     args = parser.parse_args()
     command = args.command
     if command == 'clean' or command == 'c':
         clean()
     if command == 'i':
-        _install()
+        install(args)
