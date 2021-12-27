cf4f53d02bd25287df02921ebc33261bffecb2be
huwenfan <huhuang03@gmail.com>
2021-12-27 09:32:54 +0800

    add util.reg

--- util/path/user_scope.py
+++ util/path/user_scope.py
@@ -2,6 +2,7 @@ from typing import List
 import os
 import subprocess
 from .inner.db import add
+from ..util import reg
 
 
 class UserScope:
@@ -10,28 +11,16 @@ class UserScope:
         path = path.replace('\n', '')
         return path
 
-    def _set_path(self, path: str):
-        add(path)
-        rst = subprocess.run(['powershell', '-Command', f'[Environment]::SetEnvironmentVariable("Path", "{path}", "User")'])
-        return rst
-
     def set_path(self, path: List[str]):
-        return self._set_path(";".join(path))
-
-    def get_path(self) -> [List[str], str]:
-        output = subprocess.run(['powershell', '-Command', '[Environment]::GetEnvironmentVariable("Path", "User")'], capture_output=True)
-        path = output.stdout.decode('utf-8').strip()
-        path = self.fix_path(path)
-        path_list = path.split(";")
-        return path_list, path
+        add(path)
+        return reg.save_path(path)
 
     def add_cwd_to_path(self):
-        old_path_list, old_path = self.get_path()
+        old_path_list = reg.get_path_with_de_duplicate()
         if os.getcwd() in old_path_list:
             print("already exist in path")
             return
-        new_path = old_path + ";" + os.getcwd()
-        rst = self.set_path(new_path.split(";"))
+        rst = self.set_path(old_path_list + [os.getcwd()])
         if not rst.returncode == 0:
             if rst.stderr:
                 exit(f"set new_path failed with {rst.stderr.decode('utf-8')}")
--- util/util/__init__.py
+++ util/util/__init__.py
@@ -1,2 +1,3 @@
 from .util import *
 from .util_find_program import *
+from . import reg
