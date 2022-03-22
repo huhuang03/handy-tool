02c6c21ebe849cef5ed545dd34c4ac733a69be5e
th <huhuang0@gmail.com>
2022-03-22 21:20:23 +0800

    add sync repo state

--- util/sync/repo/__init__.py
+++ util/sync/repo/__init__.py
@@ -1,5 +1,6 @@
 from .add import add
 from .repo_list import repo_list
+from .state import state
 
 
 def init_subparser(subparser):
@@ -15,5 +16,11 @@ def init_subparser(subparser):
 
     list_parser = repo_subparser.add_parser('list')
     list_parser.set_defaults(func=repo_list)
+
+    state_parser = repo_subparser.add_parser('state')
+    state_parser.set_defaults(func=state)
+
+    state_parser = repo_subparser.add_parser('st')
+    state_parser.set_defaults(func=state)
 
     subparser.add_parser('sync')
---
+++ util/sync/repo/state.py
@@ -0,0 +1,9 @@
+from . import comm
+import subprocess
+
+
+def state(args):
+    repo_list = comm.get_repo_list()
+    for repo in repo_list:
+        print(f"------in {repo}")
+        subprocess.run("git status", cwd=repo)
