4838b282b2ad33154af66aa22b7ca018f3f2c102
胡文凡 <wenfan.hu@infaith.com.cn>
2022-03-23 18:41:41 +0800

    add shell=True in util.python

--- util/sync/repo/status.py
+++ util/sync/repo/status.py
@@ -4,6 +4,10 @@ import subprocess
 
 def status(args):
     repo_list = comm.get_repo_list()
+    first = True
     for repo in repo_list:
+        if not first:
+            print("")
         print(f"------in {repo}")
-        subprocess.run("git status", cwd=repo)
+        subprocess.run("git status", shell=True, cwd=repo)
+        first = False
