0a89d82eac928d49cdc2bac00523003416783c05
th <huhuang0@gmail.com>
2022-03-27 23:14:37 +0800

    up

--- util/sync/repo/status.py
+++ util/sync/repo/status.py
@@ -1,3 +1,4 @@
+from git import Repo
 from . import comm
 import subprocess
 
@@ -8,6 +9,22 @@ def status(args):
     for repo in repo_list:
         if not first:
             print("")
-        print(f"------in {repo}")
+        print(f"-------- {repo}")
         subprocess.run("git status", shell=True, cwd=repo)
+        _check_repo(repo)
         first = False
+
+
+# I want check I have some unpushed commit
+# or uncommitted change.
+def _check_repo(repo_path):
+    """
+    :return: false if have some unpushed commit or uncommitted change.
+    """
+    repo = Repo.init(repo_path)
+    if repo.is_dirty():
+        print("is dirty!!!!!")
+        return False
+    if repo.head.ref != repo.refs['origin/master']:
+        print("look have some thing not commit!")
+    return True
