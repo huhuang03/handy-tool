8c0fbf104ea573d5539db3484bcf0b1e32426f40
胡文凡 <wenfan.hu@infaith.com.cn>
2022-03-30 18:39:06 +0800

    up

--- util/idea/idea_pycharm.py
+++ util/idea/idea_pycharm.py
@@ -3,7 +3,7 @@ from .idea_jebrain import IDeaJetBrains
 
 class IdeaPycharm(IDeaJetBrains):
     def __init__(self):
-        super().__init__("pycharm")
+        super().__init__("pycharm", mac_app_folder_name="PyCharm CE")
 
 
 def main():
