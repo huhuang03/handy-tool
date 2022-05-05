fb2877002b44e72286783f59ed052926ef6adf0d
th <huhuang0@gmail.com>
2022-05-06 03:35:55 +0800

    up

--- util/merged_rm.py
+++ util/merged_rm.py
@@ -4,27 +4,27 @@ import subprocess
 import argparse
 
 _need_check_branch = ['dev']
-_reserve_branch = ["*", "dev", "main", "master"]
+_reserve_branch = ["*", "dev", "main", "master", "release"]
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", action=argparse.BooleanOptionalAction)
+    parser.add_argument("-c", action=argparse.BooleanOptionalAction, help="compare to current branch")
 
     args = parser.parse_args()
 
-    print(args)
+    # print(args)
 
-    # output = subprocess.check_output(['git', 'branch', '--merged']).decode('utf-8')
-    # merged_branches = [item.strip() for item in output.split("\n") if item.strip()]
+    output = subprocess.check_output(['git', 'branch', '--merged']).decode('utf-8')
+    merged_branches = [item.strip() for item in output.split("\n") if item.strip()]
 
-    # for mb in merged_branches:
-    #     if not (mb.startswith('*') or mb in _reserve_branch):
-    #         print(f'delete branch {mb}')
-    #         subprocess.check_output(['git', 'branch', '-d', mb])
+    for mb in merged_branches:
+        if not (mb.startswith('*') or mb in _reserve_branch):
+            print(f'delete branch {mb}')
+            subprocess.check_output(['git', 'branch', '-d', mb])
 
-    #     if mb in _need_check_branch:
-    #         print('{} already merged'.format(mb))
+        if mb in _need_check_branch:
+            print('{} already merged'.format(mb))
 
 
 if __name__ == '__main__':
