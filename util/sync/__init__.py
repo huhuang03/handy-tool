1d8277a99b2aa682badb9f348ac21a26e5e64369
胡文凡 <wenfan.hu@infaith.com.cn>
2022-05-07 18:33:15 +0800

    "auto commit by repo check"

--- util/sync/__init__.py
+++ util/sync/__init__.py
@@ -6,6 +6,7 @@ import argparse
 from util.util import is_mac, is_windows
 from .mac import mac_sync
 from . import repo
+import urllib.request
 
 
 def _get_root_path():
@@ -38,12 +39,32 @@ def main():
     parser = argparse.ArgumentParser(description="sync util")
     subparser = parser.add_subparsers(dest="command")
     repo.init_subparser(subparser)
-    args = parser.parse_args(sys.argv[1:])
-    args.func(args)
-    # strange. how can I do this?
-    # subparser.add_parser("repo")
-    # RepoManager.init_parser(subparser)
 
-    # args = parser.parse_args()
-    # if args.command == "repo":
-    #     RepoManager(subparser).run(args)
+    args = parser.parse_args(sys.argv[1:])
+    if args.command == 'repo':
+        args.func(args)
+        return
+
+    sy_root_dir = os.path.expanduser("~/.sy")
+    os.makedirs(sy_root_dir, exist_ok=True)
+    shell = os.environ['SHELL']
+    remote_url = ""
+    if shell == "/bin/zsh":
+        remote_url = "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh"
+    else:
+        exit(f"can't handle shell: {shell}")
+
+    file_name = remote_url.split("/")[-1]
+    # download to bash.
+    urllib.request.urlretrieve(remote_url, os.path.join(sy_root_dir, file_name))
+
+    # complete the bash
+    source_command = "source ~/.sy/" + file_name
+
+    system_bash_path = os.path.expanduser("~/.zshrc")
+    with open(system_bash_path, "r") as f:
+        exist = source_command in f.read()
+
+    if not exist:
+        with open(system_bash_path, "a") as f:
+            f.writelines(["# added by .sy util\n", source_command])
