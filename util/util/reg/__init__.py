415ff7f8b646a66c512fa50cfea634d1ea1dbf35
th <huhuang0@gmail.com>
2022-06-11 23:38:37 +0800

    "auto commit by repo check"

---
+++ util/util/reg/__init__.py
@@ -0,0 +1,7 @@
+# reg is reg. nothing else. what can you provide?
+
+def get_value_current_user(sub_key: str, name: str):
+    """
+    Maybe you confused about the name, but the windows reg is like this. Ok, goon.
+    """
+    pass
