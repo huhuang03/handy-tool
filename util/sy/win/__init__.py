d6e0525edec9ec5a5318fc6c8a2efd34bc77c4e1
huhuang03 <huhuang03@gmail.com>
2022-11-13 16:57:41 +0800

    fix(sync): fix sync windows terminal

--- util/sy/win/__init__.py
+++ util/sy/win/__init__.py
@@ -1,69 +1,12 @@
-import subprocess
-import os.path
-from .. import util as _util
 from ..comm import git_sync
 from .init_wt import init_wt
-
-# set-executionpolicy RemoteSigned call this in admin mode
-_CONTENT = """
-function doJump {
-    $Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
-}
-
-function doUnJump {
-    Remove-Item Env:http_proxy
-    Remove-Item Env:https_proxy
-}
-
-New-Alias jump doJump
-New-Alias unjump doUnJump
-"""
-
-
-def _back():
-    """@:param root the source files root"""
-    profile_path = subprocess.run(["powershell", "-c", "echo $profile"], capture_output=True, text=True).stdout.strip()
-
-    sy_folder_name = ".sy"
-    root = os.path.join(os.path.dirname(profile_path), sy_folder_name)
-
-    sy_file_name = "sy.ps1"
-    sy_path = os.path.join(root, sy_file_name)
-
-    if not os.path.exists(root):
-        os.makedirs(root)
-
-    with open(sy_path, 'w') as f:
-        f.write(_CONTENT)
-
-    _util.insert_if_not_exist(profile_path, _CONTENT)
-
-
-def _create_windows_terminal_settings_link(setting_dir: str):
-    setting_file = os.path.join(setting_dir, 'settings.json')
-    # ok, create symbolic link
-    # mklink(os.path.join(os.path.dirname(__file__), 'windows_terminal_config.json'), 'test.json')
-
-
-def _init_windows_terminal():
-    print(os.path.dirname(__file__))
-    return
-    origin_folder = os.path.expanduser("~/AppData/Local/Packages/")
-    for f in os.listdir(origin_folder):
-        if f.startswith("Microsoft.WindowsTerminal"):
-            origin_folder = os.path.join(origin_folder, f, 'LocalState')
-            break
-    setting_file = os.path.join(origin_folder, 'settings.json')
-    if not os.path.exists(setting_file):
-        exit("why can't find windows.terminal settings.json")
-    # _create_windows_terminal_settings_link(origin_folder)
-    test_file_path = os.path.join(origin_folder, 'test.json')
-    print(os.path.exists(test_file_path))
-    # can I check this is a real file. not a folder?
+from .powershell import sy_powershell
+from .init_wt import sync_windows_terminal
 
 
 def win_sync():
     git_sync()
-    _init_windows_terminal()
-    # we not finish init_wt, because I can't think about how to set start fodler.
-    # init_wt()
+    print("init powershell")
+    sy_powershell()
+    print("init windows terminal")
+    sync_windows_terminal()
--- util/sy/win/init_wt.py
+++ util/sy/win/init_wt.py
@@ -1,10 +1,31 @@
 import os
-from ssl import HAS_NEVER_CHECK_COMMON_NAME
+
+def sync_windows_terminal():
+    pass
+
+
+def _init_windows_terminal():
+    print(os.path.dirname(__file__))
+    return
+    origin_folder = os.path.expanduser("~/AppData/Local/Packages/")
+    for f in os.listdir(origin_folder):
+        if f.startswith("Microsoft.WindowsTerminal"):
+            origin_folder = os.path.join(origin_folder, f, 'LocalState')
+            break
+    setting_file = os.path.join(origin_folder, 'settings.json')
+    if not os.path.exists(setting_file):
+        exit("why can't find windows.terminal settings.json")
+    # _create_windows_terminal_settings_link(origin_folder)
+    test_file_path = os.path.join(origin_folder, 'test.json')
+    print(os.path.exists(test_file_path))
+    # can I check this is a real file. not a folder?
+
 
 def init_wt():
     """Init windows terminal"""
     # how about find latest powershell
-    start_menu_folder = os.path.join("c:\\", "ProgramData", "Microsoft", 'Windows', "Start Menu", "Programs", "PowerShell")
+    start_menu_folder = os.path.join("c:\\", "ProgramData", "Microsoft", 'Windows', "Start Menu", "Programs",
+                                     "PowerShell")
     has_powershell = os.path.exists(start_menu_folder)
     if not has_powershell:
         print("can't find powershell in start menu")
@@ -13,7 +34,10 @@ def init_wt():
         for f in os.listdir(start_menu_folder):
             if "PowerShell" in f and not "preview" in f:
                 powershell_path = os.path.join(start_menu_folder, f)
-            
+
         has_powershell = not not powershell_path
 
-    
+def _create_windows_terminal_settings_link(setting_dir: str):
+    setting_file = os.path.join(setting_dir, 'settings.json')
+    # ok, create symbolic link
+    # mklink(os.path.join(os.path.dirname(__file__), 'windows_terminal_config.json'), 'test.json')
