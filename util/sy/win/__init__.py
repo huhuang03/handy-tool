a05c417ba3b13a762bb61f45736ade3c1e54ac95
huhuang03 <huhuang03@gmail.com>
2022-12-13 07:40:48 +0800

    feat(sy): update sync_windows_terminal

--- util/sy/win/__init__.py
+++ util/sy/win/__init__.py
@@ -1,7 +1,6 @@
 from ..comm import git_sync
-from .init_wt import init_wt
 from .powershell import sy_powershell
-from .init_wt import sync_windows_terminal
+from .init_wt import _init_windows_terminal
 
 
 def win_sync():
@@ -9,4 +8,4 @@ def win_sync():
     print("init powershell")
     sy_powershell()
     print("init windows terminal")
-    sync_windows_terminal()
+    _init_windows_terminal()
