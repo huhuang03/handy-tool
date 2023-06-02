62b1013c1ce8cf979d985389f49ac05f3c8f3031
胡文凡 <wenfan.hu@infaith.com.cn>
2023-06-02 09:14:58 +0800

    up

--- handy_tool/idea/idea_pycharm.py
+++ handy_tool/idea/idea_pycharm.py
@@ -3,7 +3,7 @@ from .idea_jetbrain import IDeaJetBrains
 
 class IdeaPycharm(IDeaJetBrains):
     def __init__(self):
-        super().__init__("pycharm", mac_app_folder_name="PyCharm CE")
+        super().__init__("pycharm", mac_app_folder_name="PyCharm")
 
 
 def main():
--- handy_tool/repo/add.py
+++ handy_tool/repo/add.py
@@ -9,12 +9,18 @@ def add(args):
     repo_list = comm.get_repo_list() or []
     path = os.path.abspath(repo_path)
     if not os.path.exists(path):
-        print(f"{path} is exist.")
+        print(f"{path} is not exist.")
         return
 
+    already_exist = False
     for item in repo_list:
         if item[cons.KEY_PATH] == path:
             repo_list.remove(item)
+            already_exist = True
 
     repo_list.append(repo)
     comm.save_repo_list(repo_list)
+    if already_exist:
+        print('add success(with re-add)!')
+    else:
+        print('add success!')
