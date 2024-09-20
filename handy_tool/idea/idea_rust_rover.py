80b78b60248b73a988885b66d1b0541158a85efb
胡文凡 <wenfan.hu@infaith.com.cn>
2024-09-20 11:35:49 +0800

    up

--- handy_tool/idea/idea_rust_rover.py
+++ handy_tool/idea/idea_rust_rover.py
@@ -3,7 +3,7 @@ from .idea_jetbrain import IDeaJetBrains
 
 class IdeaRustRover(IDeaJetBrains):
     def __init__(self):
-        super().__init__("RustRover", mac_app_folder_name="RustRover")
+        super().__init__("RustRover")
 
 
 def main():
