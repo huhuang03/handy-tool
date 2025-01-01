4ea4f3927e5df9f7c0cffb56fda63fb1555d883a
huhuang03 <huhuang03@gmail.com>
2025-01-01 08:55:26 +0800

    up

--- handy_tool/idea/idea_rust_rover.py
+++ handy_tool/idea/idea_rust_rover.py
@@ -3,7 +3,7 @@ from .idea_jetbrain import IDeaJetBrains
 
 class IdeaRustRover(IDeaJetBrains):
     def __init__(self):
-        super().__init__("RustRover")
+        super().__init__("RustRover", "rustrover64")
 
 
 def main():
