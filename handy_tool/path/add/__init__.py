4555f0dce899ccc60c3a7126550d92d52d3d7804
huhuang03 <huhuang03@gmail.com>
2025-09-03 10:08:49 +0800

    up

--- handy_tool/path/add/__init__.py
+++ handy_tool/path/add/__init__.py
@@ -3,5 +3,9 @@ from ...util.util import ensure_is_win
 
 
 def add():
+    """
+    this already check has added before add
+    :return:
+    """
     ensure_is_win()
     user_scope.add_cwd_to_path()
